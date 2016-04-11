11ea1b2a049017f0c37b6452fbb004f392e7bd8c
Marissa <badenm42@gmail.com>
2016-04-10 21:15:58 -0600

    Scae line insturment.

--- sources/instruments/lineinstrument.cpp
+++ sources/instruments/lineinstrument.cpp
@@ -99,11 +99,13 @@ void LineInstrument::paint(ImageArea &imageArea, bool isSecondaryColor, bool)
 
     if(mStartPoint != mEndPoint)
     {
+        painter.scale(1/imageArea.getZoomFactor(), 1/imageArea.getZoomFactor());
         painter.drawLine(mStartPoint, mEndPoint);
     }
 
     if(mStartPoint == mEndPoint)
     {
+        painter.scale(1/imageArea.getZoomFactor(), 1/imageArea.getZoomFactor());
         painter.drawPoint(mStartPoint);
     }
     imageArea.setEdited(true);
